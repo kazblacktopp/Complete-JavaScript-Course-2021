4126d18c6821a23f7758575aae5f113a4062febf
kazblacktopp <info@karenblacktopp.com>
2021-04-17 15:39:26 +1000

    Remove coding challenge instructions

--- 01-Fundamentals-Part-1/challenge2.js
+++ 01-Fundamentals-Part-1/challenge2.js
@@ -1,15 +1,3 @@
-/*
-CODING CHALLENGE #2:
-
-Use the BMI example from Challenge #1, and the code you already wrote, and improve it.
-
-Your tasks:
-
-1. Print a nice output to the console, saying who has the higher BMI. The message is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!".
-
-2. Use a template literal to include the BMI values in the outputs.Example: "Mark's BMI (28.3) is higher than John's (23.9)!".
-*/
-
 console.log(`Coding Challenge #2:`);
 
 // Test data 1:
