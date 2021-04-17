b31eefecd24d53310532ec2d55e8f158587a1a8b
kazblacktopp <info@karenblacktopp.com>
2021-04-18 09:01:54 +1000

    Remove coding challenge instructions

--- 02-Fundamentals-Part-2/challenge1.js
+++ 02-Fundamentals-Part-2/challenge1.js
@@ -1,54 +1,35 @@
-
-// CODING CHALLENGE #1
-
-// Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
-
-// Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
-
-// A team only wins if it has at least double the average score of the other team. Otherwise, no team wins!
-
-// Your tasks:
-
-// 1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
-
-// 2. Use the function to calculate the average for both teams
-
-// 3. Create a function 'checkWinner' that takes the average score of each team as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner to the console, together with the victory points, according to the rule above.
-
-// Example: "Koalas win (30 vs. 13)"
-
-// 4. Use the 'checkWinner' function to determine the winner for both Data 1 and Data 2
-
-// 5. Ignore draws this time
-
-// Test data:
-
-// DATA 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
-
-// DATA 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
-
 const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 
 // TEST DATA 1:
-let avgDolphins = calcAverage(44, 23, 71);
-let avgKoalas = calcAverage(65, 54, 49);
 
-const checkWinner = (avgDolphins, avgKoalas) => {
-    if (avgDolphins >= avgKoalas * 2) {
-        return `Dolphins win (${avgDolphins} vs. ${avgKoalas})`;
-    } else if (avgKoalas >= avgDolphins * 2) {
-        return `Koalas win (${avgKoalas} vs. ${avgDolphins})`;
+let avgScoreDolphins = calcAverage(44, 23, 71);
+let aveScoreKoalas = calcAverage(65, 54, 49);
+
+const checkWinner = (avgScoreDolphins, aveScoreKoalas
+) => {
+    if (avgScoreDolphins >= aveScoreKoalas
+        * 2) {
+        return `Dolphins win 🏆 (${avgScoreDolphins} vs. ${aveScoreKoalas
+            })`;
+    } else if (aveScoreKoalas
+        >= avgScoreDolphins * 2) {
+        return `Koalas win 🏆 (${aveScoreKoalas
+            } vs. ${avgScoreDolphins})`;
     } else {
         return `No team wins..`;
     }
 }
-console.log(avgDolphins, avgKoalas);
-console.log(checkWinner(avgDolphins, avgKoalas));
+console.log(avgScoreDolphins, aveScoreKoalas
+);
+console.log(checkWinner(avgScoreDolphins, aveScoreKoalas
+));
 
 // TEST DATA 2:
 
-avgDolphins = calcAverage(85, 54, 41);
-avgKoalas = calcAverage(23, 34, 27);
+avgScoreDolphins = calcAverage(85, 54, 41);
+aveScoreKoalas = calcAverage(23, 34, 27);
 
-console.log(avgDolphins, avgKoalas);
-console.log(checkWinner(avgDolphins, avgKoalas));
+console.log(avgScoreDolphins, aveScoreKoalas
+);
+console.log(checkWinner(avgScoreDolphins, aveScoreKoalas
+));
