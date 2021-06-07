852946adc42b3273261826d0ca50f432da5ab79a
kazblacktopp <karen.blacktopp@gmail.com>
2021-06-07 15:59:25 +1000

    Add full modal functionality

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -5,7 +5,30 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsShowModal = document.querySelectorAll(".show-modal");
 
-console.log(btnsShowModal);
+function eventListener(event, selector) {
+  if (event === "click") {
+    selector.addEventListener("click", closeModal);
+  } else {
+    selector.addEventListener("keydown", function (event) {
+      if (event.key === "Escape" && !modal.classList.contains("hidden"))
+        closeModal();
+    });
+  }
+}
+
+function openModal() {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+
+function closeModal() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
 
 for (let i = 0; i < btnsShowModal.length; i++)
-  console.log(btnsShowModal[i].textContent);
+  btnsShowModal[i].addEventListener("click", openModal);
+
+eventListener("click", btnCloseModal);
+eventListener("click", overlay);
+eventListener("keydown", document);
