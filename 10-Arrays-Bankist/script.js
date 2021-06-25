e656fc7af24a02947cd9173f8d9fce63c5f8f8bc
kazblacktopp <karen.blacktopp@gmail.com>
2021-06-25 18:10:58 +1000

    Add rounding function and applied fixed decimal place for printed values

--- 10-Arrays-Bankist/script.js
+++ 10-Arrays-Bankist/script.js
@@ -7,9 +7,22 @@
 // Data
 const account1 = {
   owner: "Jonas Schmedtmann",
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+
+  movementsDates: [
+    "2019-11-18T21:31:17.178Z",
+    "2019-12-23T07:42:02.383Z",
+    "2020-01-28T09:15:04.904Z",
+    "2020-04-01T10:17:24.185Z",
+    "2020-05-08T14:11:59.604Z",
+    "2020-05-27T17:01:17.194Z",
+    "2020-07-11T23:36:17.929Z",
+    "2020-07-12T10:51:36.790Z",
+  ],
+  currency: "EUR",
+  locale: "pt-PT", // de-DE
 };
 
 const account2 = {
@@ -17,23 +30,22 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    "2019-11-01T13:15:33.035Z",
+    "2019-11-30T09:48:16.867Z",
+    "2019-12-25T06:04:23.907Z",
+    "2020-01-25T14:18:46.235Z",
+    "2020-02-05T16:33:06.386Z",
+    "2020-04-10T14:43:26.374Z",
+    "2020-06-25T18:49:59.371Z",
+    "2020-07-26T12:01:20.894Z",
+  ],
+  currency: "USD",
+  locale: "en-US",
 };
 
-const account3 = {
-  owner: "Steven Thomas Williams",
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
-
-const account4 = {
-  owner: "Sarah Smith",
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
+const accounts = [account1, account2];
 let activeAccount = {};
 let sorted = false;
 
@@ -84,7 +96,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     }: ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
    `;
 
@@ -105,23 +117,33 @@ const createUsername = accts => {
 
 createUsername(accounts);
 
+// Function to round number to 2 decimal places
+const round = num => {
+  const roundedNum = Math.round((num * 100).toPrecision(15)) / 100;
+  return roundedNum;
+};
+
 const calcDisplaySummary = acc => {
   const sumIn = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
 
-  labelSumIn.textContent = `${sumIn}€`;
+  labelSumIn.textContent = `${sumIn.toFixed(2)}€`;
 
-  labelSumOut.textContent = `${Math.abs(
-    acc.movements.filter(mov => mov < 0).reduce((acc, mov) => acc + mov, 0)
-  )}€`;
+  const sumOut = acc.movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
 
-  labelSumInterest.textContent = `${(sumIn * acc.interestRate) / 100}€`;
+  labelSumOut.textContent = `${Math.abs(sumOut).toFixed(2)}€`;
+
+  const sumInterest = round((sumIn * acc.interestRate) / 100);
+
+  labelSumInterest.textContent = `${sumInterest.toFixed(2)}€`;
 };
 
 const calcDisplayBalance = acct => {
   acct.balance = acct.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acct.balance}€`;
+  labelBalance.textContent = `${acct.balance.toFixed(2)}€`;
 };
 
 // Event handler
@@ -131,7 +153,7 @@ btnLogin.addEventListener("click", function (e) {
     acct => acct.username === inputLoginUsername.value.toLowerCase()
   );
 
-  if (activeAccount?.pin === Number(inputLoginPin.value)) {
+  if (activeAccount?.pin === +inputLoginPin.value) {
     inputLoginUsername.value = inputLoginPin.value = "";
     inputLoginUsername.blur();
     inputLoginPin.blur();
@@ -154,7 +176,7 @@ btnTransfer.addEventListener("click", e => {
   const recipient = accounts.find(
     acct => acct.username === inputTransferTo.value.toLowerCase()
   );
-  const transAmount = Number(inputTransferAmount.value);
+  const transAmount = round(inputTransferAmount.value);
 
   if (
     transAmount > 0 &&
@@ -172,10 +194,13 @@ btnTransfer.addEventListener("click", e => {
 
 btnLoan.addEventListener("click", e => {
   e.preventDefault();
-  const loan = Number(inputLoanAmount.value);
+  const loanAmount = round(inputLoanAmount.value);
 
-  if (loan > 0 && activeAccount.movements.some(mov => mov >= loan * 0.1)) {
-    activeAccount.movements.push(loan);
+  if (
+    loanAmount > 0 &&
+    activeAccount.movements.some(mov => mov >= loanAmount * 0.1)
+  ) {
+    activeAccount.movements.push(loanAmount);
     displayUI(activeAccount);
   }
   inputLoanAmount.value = "";
@@ -185,7 +210,7 @@ btnClose.addEventListener("click", e => {
   e.preventDefault();
 
   const user = inputCloseUsername.value.toLowerCase();
-  const userPin = Number(inputClosePin.value);
+  const userPin = +inputClosePin.value;
 
   if (activeAccount.username === user && activeAccount.pin === userPin) {
     const confirmation = confirm(
